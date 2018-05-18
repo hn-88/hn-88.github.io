7dd106a64b58f8a453f29a2e5555be27d159737f
hn-88 <hn-88@users.noreply.github.com>
2018-05-18 14:43:38 +0530

    populating dir

---
+++ home_files/stMainLinks.js
@@ -0,0 +1,23 @@
+oCMenu.makeMenu('top1','','&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;','','','118')
+	oCMenu.makeMenu('sub1','top1','&nbsp;Introduction','Pages/SriSathyaSaiBaba/Introduction.htm','','110')
+	oCMenu.makeMenu('sub2','top1','&nbsp;Life History','Pages/SriSathyaSaiBaba/LifeHistory.htm','','110')
+	oCMenu.makeMenu('sub3','top1','&nbsp;His Writings','Pages/SriSathyaSaiBaba/HisWritings.htm','','110')
+	oCMenu.makeMenu('sub4','top1','&nbsp;Baba on Himself','Pages/SriSathyaSaiBaba/BabaHimself.htm','','110')
+
+oCMenu.makeMenu('top5','','','','','118')
+
+oCMenu.makeMenu('top2','','&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;','','','118')
+	oCMenu.makeMenu('top2-1','top2','&nbsp;Introduction','Pages/Service_Projects/Introduction.htm','','150')
+	oCMenu.makeMenu('top2-2','top2','&nbsp;Education','Pages/Service_Projects/Education.htm','','150')
+	oCMenu.makeMenu('top2-3','top2','&nbsp;Healthcare','http://sssmt.org.in/pages/index.asp','','150')
+	oCMenu.makeMenu('top2-4','top2','&nbsp;Water Supply','Pages/Service_Projects/WaterSupply.htm','','150')
+	oCMenu.makeMenu('top2-5','top2','&nbsp;Relief to the Poor','Pages/Service_Projects/ReliefPoor.htm','','150')
+	oCMenu.makeMenu('top2-6','top2','&nbsp;Culture and Heritage','Pages/Service_Projects/Culuture_and_Heritage.htm','','150')
+
+oCMenu.makeMenu('top4','','&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;','','','118')
+	oCMenu.makeMenu('top4-1','top4','&nbsp;Prasanthi Nilayam','Pages/AshramInfo/Ashram.htm','','120')
+	oCMenu.makeMenu('top4-2','top4','&nbsp;Brindavan','Pages/AshramInfo/Brindavan.htm','','120')
+	oCMenu.makeMenu('top4-3','top4','&nbsp;Kodaikanal','Pages/AshramInfo/Kodai.htm','','120')
+
+//Leave these two lines! Making the styles and then constructing the menu
+oCMenu.makeStyle(); oCMenu.construct()
